30c5d3b2fda6a0b012e8790d3cebc36970ffcca0
Dipasree Poddar <dipasreep2016@email.iimcal.ac.in>
2022-07-17 22:43:01 +0530

    Working version for deployment

--- hooks/useIPFS.js
+++ hooks/useIPFS.js
@@ -1,13 +1,7 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 
 const useIPFS = (hash, filename) => {
-  const [file, setFile] = useState(null);
-
-  useEffect(() => {
-    setFile(`https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`);
-  }, []);
-
-  return file;
+  return `https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`;
 };
 
 export default useIPFS;
